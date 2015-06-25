80a7b14c245e3ed3a336bbdf066ec49f1df1126a
sx1989827 <395414574@qq.com>
2015-06-25 14:52:46 +0800

    add

---
+++ ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EasyUIControl
+//
+//  Created by 孙昕 on 15/6/25.
+//  Copyright (c) 2015年 孙昕. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
